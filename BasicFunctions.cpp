9d2ca56a64c52c8f8b2f741641614cb9db2fcd1b
Bryan Stanfill <stanfill@iastate.edu>
2013-04-22 13:16:19 -0500

    make distance and cd functions

--- BasicFunctions.cpp
+++ BasicFunctions.cpp
@@ -19,6 +19,35 @@ arma::vec meanQ4C(arma::mat Q) {
   return qhat;
 } 
 
+// [[Rcpp::export]]
+double RdistC(NumericVector Q1, NumericVector Q2){
+	/*Compute the geodesic distance between quaternions Q1 and Q2*/
+	double cp = sum(Q1*Q2);
+	return acos(2*cp*cp-1);
+	
+}
+
+// [[Rcpp::export]]
+NumericVector cdfunsC(NumericMatrix Qs, NumericVector Qhat){
+	int n = Qs.nrow();
+	double crs;
+	NumericVector cds(2);
+	NumericVector rs(n);
+	
+	for(int i=0; i<n; i++){
+		rs[i] = 2*acos(Qs(i,0));
+		
+		crs = cos(rs[i]);
+		
+		cds[0] += 1-pow(crs,2);
+		cds[1] += 1+2*crs;
+	}
+	
+	cds[0] = (2*cds[0])/(3*n);
+	cds[1] = (cds[1]/(3*n));
+	
+	return cds;
+}
 
 // [[Rcpp::export]]
 NumericVector bootQhat(NumericMatrix Q){
@@ -41,23 +70,30 @@ NumericVector bootQhat(NumericMatrix Q){
 
 /*** R
 
-#library(Rcpp)
-#library(microbenchmark)
+library(microbenchmark)
 library(rotations)
-Qs<-ruars(10,rcayley,space='Q4')
-bootQhat(Qs)
+rs<-rcayley(20)
+Qs<-genR(rs,space='Q4')
+rs
+angle(Qs)
+cdfunsC(Qs,id.Q4)
+
+#Qs<-ruars(10,rcayley,space='Q4')
+
+#Qhat<-as.Q4(meanQ4C(Qs))
+#Qhat
+#mean(Qs)
+
+#dist(Qhat,method='intrinsic')
+#RdistC(Qhat,id.Q4)
 
 #tim<-microbenchmark(          
-#mean(Qs),
-#meanQ4C(Qs)
+#dist(Qhat,method='intrinsic'),
+#RdistC(Qhat,id.Q4)
 #)
 
 #print(tim)
 #plot(tim)
-
-#Q1<-matrix(rnorm(16),4,4)
-#Q1%*%Q1
-#qhatC(Q1)
 
 */
 
