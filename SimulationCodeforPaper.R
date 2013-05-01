52f60f05ebffb385a0d453a6abbd1ebdf48a5444
Bryan Stanfill <stanfill@iastate.edu>
2013-05-01 14:41:33 -0500

    further beautifying ECDF plots

--- SimulationCodeforPaper.R
+++ SimulationCodeforPaper.R
@@ -199,17 +199,21 @@ chiDF<-data.frame(kappa=rep(ks,1000),n='Chisq',variable='Tr',value=rep(ss,each=2
 chiDF$ID<-paste(chiDF$kappa,chiDF$n)
 
 fullDF<-rbind(resM,chiDF)
+
 Newlabs<-c("Chisq","10","50","100","300")
 fullDF$n<-factor(fullDF$n,levels=Newlabs)
 fullDF$Stat<-1
 fullDF[fullDF$n=='Chisq',]$Stat<-2
 fullDF$Stat<-as.factor(fullDF$Stat)
+
 if(Dist=='cayley'){
+	
+	fullDF$kappa<-factor(fullDF$kappa,labels=c("kappa == 0.5","kappa == 8.0"))
 	
 	qplot(value,Prob,data=fullDF,colour=n,lwd=Stat,geom="line",xlab='x',ylab="F(x)",xlim=c(0,15))+
 		scale_colour_grey("",labels=c(expression(chi[3]^2),"n=10","n=50","n=100","n=300"))+
-		facet_grid(.~kappa,labeller=label_both)+theme_bw()+coord_fixed(ratio=15/1)+
-    scale_size_discrete("",range=c(1,2),guide='none')+
+		facet_grid(.~kappa,labeller=label_parsed)+theme_bw()+coord_fixed(ratio=15/1)+
+    scale_size_discrete(range=c(0.75,1.5),guide='none')+
 	  guides(colour=guide_legend(label.hjust=0))
 	
 	#setwd("C:/Users/stanfill/Dropbox/Thesis/Intervals/Figures")
@@ -218,10 +222,12 @@ if(Dist=='cayley'){
 	
 }else if(Dist=='fisher'){
 	
+	fullDF$kappa<-factor(fullDF$kappa,labels=c("kappa == 0.5","kappa == 8.0"))
+	
 	qplot(value,Prob,data=fullDF,colour=n,lwd=Stat,geom="line",xlab='x',ylab="F(x)",xlim=c(0,15))+
 		scale_colour_grey("",labels=c(expression(chi[3]^2),"n=10","n=50","n=100","n=300"))+
-		facet_grid(.~kappa,labeller=label_both)+theme_bw()+coord_fixed(ratio=15/1)+
-	  scale_size_discrete("",range=c(1,2),guide='none')+
+		facet_grid(.~kappa,labeller=label_parsed)+theme_bw()+coord_fixed(ratio=15/1)+
+	  scale_size_discrete(range=c(0.75,1.5),guide='none')+
 	  guides(colour=guide_legend(label.hjust=0))
 	
 	#setwd("C:/Users/stanfill/Dropbox/Thesis/Intervals/Figures")
@@ -230,11 +236,13 @@ if(Dist=='cayley'){
 	
 }else{
 	
+	fullDF$kappa<-factor(fullDF$kappa,labels=c("kappa == 0.05","kappa == 8.0"))
+	
 	qplot(value,Prob,data=fullDF,colour=n,lwd=Stat,geom="line",xlab='x',ylab="F(x)",xlim=c(0,15))+
 		scale_colour_grey("",labels=c(expression(chi[3]^2),"n=10","n=50","n=100","n=300"))+
-		facet_grid(.~kappa,labeller=label_both)+theme_bw()+coord_fixed(ratio=15/1)+
+		facet_grid(.~kappa,labeller=label_parsed)+theme_bw()+coord_fixed(ratio=15/1)+
 	  guides(colour=guide_legend(label.hjust=0))+
-	  scale_size_discrete("",range=c(1,2),guide='none')
+	  scale_size_discrete("",range=c(0.75,1.5),guide='none')
 
 	#setwd("C:/Users/stanfill/Dropbox/Thesis/Intervals/Figures")
 	#ggsave("vonMisesECDF.pdf",height=5,width=8)
