bd19706b141e4b528105e7fcec88ff545ecd85a4
Bryan Stanfill <stanfill@iastate.edu>
2013-06-19 14:25:51 -0500

    fold is.SO3 and as.SO3 into SO3 man file

--- rotations2/R/parameterizations.R
+++ rotations2/R/parameterizations.R
@@ -21,11 +21,11 @@ setOldClass("Q4")
 
 #' Quaternions
 #' 
-#' Creates or tests for objects of type 'Q4'
+#' Creates or tests for objects of type "Q4"
 #' 
 #' Construct a unit quaternion to represent a rotation.  Each quaternion can be interpreted as a rotation of some reference frame 
-#' about the axis U (of unit length) through the angle theta.  For each axis, u, and angle theta the quaternion is formed through
-#' \deqn{q=[cos(\theta/2),sin(\theta/2)u]^\top.}{q=[cos(theta/2),sin(theta/2)u]'.}  If the theta element is left empty then the 
+#' about the axis U (of unit length) through the angle theta.  For each axis, U, and angle theta the quaternion is formed through
+#' \deqn{q=[cos(\theta/2),sin(\theta/2)U]^\top.}{q=[cos(theta/2),sin(theta/2)U]'.}  If the theta element is left empty then the 
 #' Frobenius-norm of each axis is taken to the the angle theta.  If an \code{\link{SO3}} object is given then this function will
 #' return the quaternion equivalent.
 #'
@@ -33,7 +33,7 @@ setOldClass("Q4")
 #' @param q object to be coerced or tested
 #' @param U n-by-3 matrix where rows represent axes of rotation
 #' @param theta vector of rotation angles
-#' @param R matrix in SO(3) to be translated into quaternions
+#' @param R n-by-9 matrix of rotation matrices to be translated into quaternions
 #' @param ... additional arguments
 #' @return unit quaternion of class Q4
 #' @family Q4
@@ -121,28 +121,48 @@ id.Q4 <- as.Q4(matrix(c(1,0,0,0),1,4))
 
 
 
-#' Matrix in SO(3)
+#' Rotation Matrices
 #' 
-#' Rotation matrix construction
+#' Creates or tests for objects of type "SO3"
 #' 
-#' Construct a 3-by-3 non-singular orthogonal matrix with determinant one.  The construction is accodring to Rodrigues
-#' formula.  It is interpreted as a rotation of all three axis of the identity matrix about the axis U (of unit length)
-#' through the angle theta.  Alternatively, if U is not of unit length, the length of U is taken to be angle of 
-#' rotation, theta.  Given a quaternion representation of a rotation this function will translate the quaternion
-#' into a rotation matrix
+#' Construct a unit quaternion to represent a rotation.  Each quaternion can be interpreted as a rotation of some reference frame 
+#' about the axis U (of unit length) through the angle theta.  For each axis, U, and angle theta the quaternion is formed through
+#' \deqn{exp[\Phi(Ur)]}{q=[cos(theta/2),sin(theta/2)u]'.}  If the theta element is left empty then the 
+#' Frobenius-norm of each axis is taken to the the angle theta.  If an \code{\link{SO3}} object is given then this function will
+#' return the quaternion equivalent.
 #'
 #' @export
-#' @param U three-dimensional vector describing the axis of rotation
-#' @param theta vector of angles to create the matrices
-#' @param q quaternions to be translated into rotations
+#' @param R object to be coerced or tested
+#' @param U n-by-3 matrix where rows represent axes of rotation
+#' @param theta vector of rotation angles
+#' @param q n-by-4 matrix of quaternions to be translated into rotations
 #' @param ... additional arguments
 #' @return matrix of rotations in SO3 format
 #' @family SO3
 
-SO3 <- function(U,...){
+SO3 <- function(R,...){
   UseMethod("SO3")
 }
 
+#' @rdname SO3
+#' @family SO3
+
+as.SO3<-function(R){
+	class(R)<-"SO3"
+	return(R)
+}
+
+#' @rdname SO3
+#' @family SO3
+
+is.SO3 <- function(R) {
+	
+	x <- matrix(R, 3, 3)
+	if (any(is.na(R))) return(FALSE)
+	
+	return(all(sum(t(R) %*% R - diag(1, 3))<10e-10)) 
+	
+}
 
 #' @rdname SO3
 #' @method SO3 default
@@ -204,17 +224,7 @@ SO3.Q4<-function(q){
 }
 
 
-#' Add SO3 class to object
-#' 
-#' @param x an R object
-#' @return x with class "SO3"
-#' @family SO3
-#' @export
 
-as.SO3<-function(x){
-  class(x)<-"SO3"
-  return(x)
-}
 
 #' Identity in SO(3) space
 #' @family SO3
@@ -222,21 +232,5 @@ as.SO3<-function(x){
 id.SO3 <- as.SO3(diag(c(1,1,1)))
 
 
-#' A function to determine if a given object is in \eqn{SO(3)} or not.
-#'
-#' @param x numeric 3-by-3 matrix or vector of length 9
-#' @return logical T if the matrix is in SO(3) and false otherwise
-#' @family SO3
-#' @export
-#' @examples
-#' is.SO3(diag(1,3,3))
-#' is.SO3(1:9)
-is.SO3 <- function(x) {
-  
-  x <- matrix(x, 3, 3)
-  if (any(is.na(x))) return(FALSE)
-  
-  return(all(sum(t(x) %*% x - diag(1, 3))<10e-10)) 
-  
-}
+
 
