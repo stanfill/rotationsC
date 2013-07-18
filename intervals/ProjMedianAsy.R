589da31b0eaf4b4d707c74a603f7a9c02ef39ac7
Bryan Stanfill <stanfill@iastate.edu>
2013-07-18 15:06:27 -0500

    change kappas to nus in simulations

--- intervals/ProjMedianAsy.R
+++ intervals/ProjMedianAsy.R
@@ -73,30 +73,37 @@ setwd("C:/Users/stanfill/Desktop/GitHub/rotationsC/intervals")
 source("IntervalFuns.R")
 
 n<-c(10,50,100,300)
-ks<-c(.05,8)
+nus<-c(.25,.75)
 B<-1000				#Number of samples to use to estimate CDF
 Dist<-c('cayley','fisher','mises')
 
 
-simSize<-length(n)*length(ks)*length(Dist)
+simSize<-length(n)*length(nus)*length(Dist)
 
 tMat<-matrix(0,simSize,B)
 
-cdfDF<-data.frame(expand.grid(kappa=ks,n=n,Dist=Dist),tMat)
+cdfDF<-data.frame(expand.grid(nus=nus,n=n,Dist=Dist),tMat)
 
 for(j in 1:simSize){
 	
 	if(cdfDF$Dist[j]=='cayley'){
+		
 		rangle<-rcayley
+		kappaj <- cayley_kappa(cdfDF$nu[j])
+		
 	}else if(cdfDF$Dist[j]=='fisher'){
+		
 		rangle<-rfisher
+		kappaj <- fisher_kappa(cdfDF$nu[j])
+		
 	}else{
 		rangle<-rvmises
+		kappaj <- vmises_kappa(cdfDF$nu[j])
 	}
 	
 	for(i in 1:B){
 		
-		rs<-rangle(cdfDF$n[j],kappa=cdfDF$kappa[j])
+		rs<-rangle(cdfDF$n[j],kappa=kappaj)
 		
 		Rs<-genR(rs)
 		
@@ -118,13 +125,13 @@ for(j in 1:simSize){
 	}
 }
 
-resM<-melt(cdfDF,id=c("Dist","kappa","n"))
-ss<-seq(0,max(resM$value),length=B)
+resM<-melt(cdfDF,id=c("Dist","nus","n"))
+ss<-seq(0,15,length=B)
 Probs<-pchisq(ss,3)
 
 resM$n<-as.factor(resM$n)
 resM$Prob<-0
-resM$ID<-paste(resM$Dist,resM$kappa,resM$n)
+resM$ID<-paste(resM$Dist,resM$nu,resM$n)
 kns<-unique(resM$ID)
 
 for(i in 1:length(unique(resM$ID))){
@@ -132,8 +139,8 @@ for(i in 1:length(unique(resM$ID))){
 	resM[resM$ID==kns[i],]$Prob<-ecdf(resM[resM$ID==kns[i],]$value)
 }
 
-chiDF<-data.frame(Dist="All",kappa=rep(ks,B),n='Chisq',variable='Tr',value=rep(ss,each=2),Prob=rep(pchisq(ss,3),each=2))
-chiDF$ID<-paste(chiDF$Dist,chiDF$kappa,chiDF$n)
+chiDF<-data.frame(Dist="All",nus=rep(nus,B),n='Chisq',variable='Tr',value=rep(ss,each=2),Prob=rep(pchisq(ss,3),each=2))
+chiDF$ID<-paste(chiDF$Dist,chiDF$nus,chiDF$n)
 
 fullDF<-rbind(resM,chiDF)
 
@@ -142,34 +149,34 @@ fullDF$n<-factor(fullDF$n,levels=Newlabs)
 fullDF$Stat<-1
 fullDF[fullDF$n=='Chisq',]$Stat<-2
 fullDF$Stat<-as.factor(fullDF$Stat)
-fullDF$kappa<-factor(fullDF$kappa,labels=c("kappa == 0.5","kappa == 8.0"))
+fullDF$nus<-factor(fullDF$nus,labels=c("nu == 0.25","nu == 0.75"))
 	
 
 qplot(value,Prob,data=fullDF[fullDF$Dist%in%c("cayley",'All'),],colour=n,lwd=Stat,geom="line",xlab='x',ylab="F(x)",xlim=c(0,15))+
 	scale_colour_grey("",labels=c(expression(chi[3]^2),"n=10","n=50","n=100","n=300"))+
-	facet_grid(.~kappa,labeller=label_parsed)+theme_bw()+coord_fixed(ratio=15/1)+
+	facet_grid(.~nus,labeller=label_parsed)+theme_bw()+coord_fixed(ratio=15/1)+
 	scale_size_discrete(range=c(0.75,1.5),guide='none')+
 	guides(colour=guide_legend(label.hjust=0))
 	
 #setwd("C:/Users/stanfill/Dropbox/Thesis/Intervals/Figures")
-#ggsave("CayleyECDF.pdf",height=5,width=8)
+#ggsave("CayleyECDFMedian.pdf",height=5,width=8)
 #write.csv(fullDF,"CayleyECDF.csv")
 	
 	
 qplot(value,Prob,data=fullDF[fullDF$Dist%in%c("fisher",'All'),],colour=n,lwd=Stat,geom="line",xlab='x',ylab="F(x)",xlim=c(0,15))+
 	scale_colour_grey("",labels=c(expression(chi[3]^2),"n=10","n=50","n=100","n=300"))+
-	facet_grid(.~kappa,labeller=label_parsed)+theme_bw()+coord_fixed(ratio=15/1)+
+	facet_grid(.~nus,labeller=label_parsed)+theme_bw()+coord_fixed(ratio=15/1)+
 	scale_size_discrete(range=c(0.75,1.5),guide='none')+
 	guides(colour=guide_legend(label.hjust=0))
 	
 #setwd("C:/Users/stanfill/Dropbox/Thesis/Intervals/Figures")
-#ggsave("FisherECDF.pdf",height=5,width=8)
+#ggsave("FisherECDFMedian.pdf",height=5,width=8)
 #write.csv(fullDF,"FisherECDF.csv")
 	
 
 qplot(value,Prob,data=fullDF[fullDF$Dist%in%c("mises",'All'),],colour=n,lwd=Stat,geom="line",xlab='x',ylab="F(x)",xlim=c(0,15))+
 	scale_colour_grey("",labels=c(expression(chi[3]^2),"n=10","n=50","n=100","n=300"))+
-	facet_grid(.~kappa,labeller=label_parsed)+theme_bw()+coord_fixed(ratio=15/1)+
+	facet_grid(.~nus,labeller=label_parsed)+theme_bw()+coord_fixed(ratio=15/1)+
 	guides(colour=guide_legend(label.hjust=0))+
 	scale_size_discrete("",range=c(0.75,1.5),guide='none')
 	
