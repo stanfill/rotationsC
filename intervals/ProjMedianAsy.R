7f7c8dc1ddb6c6237dcd1709a84777b22de87be2
Bryan Stanfill <stanfill@iastate.edu>
2013-07-18 11:15:49 -0500

    add mathematica code, try new c/d vals

--- intervals/ProjMedianAsy.R
+++ intervals/ProjMedianAsy.R
@@ -1,18 +1,21 @@
 library(rotations2)
 source("IntervalFuns.R")
 B<-500
-n<-c(10,100)
-kap<-10
-tstats<-matrix(0,B,2)
+n<-c(10,50,100)
+numN<-length(n)
+kap<-100
+tstats<-matrix(0,B,numN)
 
-for(j in 1:2){
+for(j in 1:numN){
 	
 	for(i in 1:B){
-		rs<-rcayley(n[j],kappa=kap)
+		rs<-rvmises(n[j],kappa=kap)
 		Rs<-genR(rs)
-	
+		
+		#ars<-abs(rs)
+		
 		cos2rs<-cos(rs/2)^2
-		cosrs<-cos(rs)
+		drs<-(3*cos(rs)+1)/(sin(rs/2)*12*sqrt(2))
 		#cosrs2<-cos(rs/2)^2
 		#cotrs<-cot(rs)
 
@@ -20,11 +23,11 @@ for(j in 1:2){
 		#ecos2<-mean(cosrs2)
 		#ecot<-mean(cotrs)
 
-		c<-(1/3)*(ecos2)  #I think this is C for proj median according to notes from 7/16
+		c<-(2/3)*(ecos2)  #I think this is C for proj median according to notes from 7/16
+		#c<-1
 
-		#d<--(1/sqrt(72))*(mean(cos(rs/2)/tan(rs/2)))
-		d<-(1+mean(cosrs))#/(sqrt(2))
-
+		#d<--mean(drs)
+		d<-1
 		Shat<-median(Rs)
 	
 		ShatMedian<-dist(Shat,method='intrinsic',p=2)
@@ -39,28 +42,15 @@ for(j in 1:2){
 #ses<-seq(0,max(tstats),length=B)
 #lines(ses,dchisq(ses,3))
 
-tstats[,1]<-sort(tstats[,1])
-tstats[,2]<-sort(tstats[,2])
+for(j in 1:numN){
+	tstats[,j]<-sort(tstats[,j])
+}
 plot(tstats[,1],ecdf(tstats[,1]),type='l')
-lines(tstats[,2],ecdf(tstats[,2]),col=2)
-lines(tstats[,1],pchisq(tstats[,1],3),col=3)
 
+for(j in 2:numN){
+	lines(tstats[,j],ecdf(tstats[,j]),col=(j+1))
+}
 
-####################
-#Just check c:
-
-n=50
-kap=50
-rs<-rcayley(n,kappa=kap)
-Rs<-genR(rs)
-
-Shat<-median(Rs)
-chat<-2*mean(cos(rs)^2)/3
-
-dists<-dist(Rs,Shat,method='projected')
-
-#chat and mean(dists^2) should be close
-mean(dists^2)
-chat
+lines(tstats[,1],pchisq(tstats[,1],3),lty=2)
 
 
