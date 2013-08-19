703e2782c82492e0dc57213cbceba69c03b85f9a
Bryan Stanfill <stanfill@iastate.edu>
2013-08-19 14:26:08 -0500

    take code from Heike to analyze nickel data

--- intervals/DataExample.R
+++ intervals/DataExample.R
@@ -16,7 +16,57 @@ qplot(Subject,Replicate,data=drilldata)+facet_grid(Joint~Position,labeller=label
 #Try the nickel data of Bingham's Disertation
 ############
 library(ggplot2)
-data(nickel)
-nickel$location<-as.factor(paste(nickel$xpos,nickel$ypos))
-rep1<-nickel[nickel$rep==1,]
-qplot(xpos,ypos,data=rep1)
+library(plyr)
+library(reshape2)
+load("/Users/stanfill/Dropbox/Rotation matrices/Melissa data/datasetnickel.RData")
+
+dat.out <- adply(data, .margins= c(1,3), function(x) {
+  as.vector(x)
+})
+names(dat.out)[c(1,2)] <- c("rep", "location")
+dat.out$xpos <- xpos[dat.out$location]
+dat.out$ypos <- ypos[dat.out$location]
+## convert data into a data frame including location and position information
+head(dat.out)
+
+checks <- adply(dat.out, .margins=1, function(x) {
+  is.SO3(unlist(x[3:11]))
+})
+
+dat.out$check <- checks$V1
+
+dat.ests <- dlply(dat.out, .(location), function(x) {
+  res <- na.omit(x)
+  res <- subset(res, check==TRUE)
+  
+  n <- nrow(res) 
+  SE2  <- SE1  <- NULL
+  if (n == 1) {
+    R <- as.SO3(matrix(unlist(res[1,3:11]), 3, 3))
+    SE2  <- SE1  <- R
+  } else if (n > 0) {
+    rots <- as.SO3(as.matrix(res[,3:11]))
+    SE2 <- mean(rots)
+    SE1 <- median(rots)
+  }
+  location <- as.numeric(as.character(unique(x$location)))
+  return(list(location=location, n=n, SE2=SE2, SE1=SE1))
+})
+
+loc.stats <- ldply(dat.ests, function(x) {  
+  location <- as.numeric(as.character(unique(x$location)))
+  if (x$n > 0)
+    data.frame(location=x$location, n=x$n, 
+               dE1=angle(x$SE1), dE2=angle(x$SE2),
+               dE=dist(x$SE1, x$SE2, method="intrinsic", p=1)
+    )
+})
+
+loc.stats$xpos <- xpos[loc.stats$location]
+loc.stats$ypos <- ypos[loc.stats$location]
+
+qplot(xpos,ypos,colour=dE,data=loc.stats,size=I(5))
+
+badEgg<-loc.stats[which.max(loc.stats$dE),]
+badRots<-as.SO3(data.matrix(dat.out[dat.out$location==698,3:11]))
+plot(badRots,center=median(badRots),show_estimates=c('proj.mean','proj.median'))
