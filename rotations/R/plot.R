ce4ab01dfe65e00e38d15248a0e2d121a4d79e49
Bryan Stanfill <stanfill@iastate.edu>
2013-10-17 17:04:16 -0500

    allow for all three axis to be dispalyed at once

--- rotations/R/plot.R
+++ rotations/R/plot.R
@@ -102,6 +102,7 @@ roteye <- function(origin, center, column=1) {
 #' @export
 #' 
 pointsXYZ <- function(data, center, column=1) {
+  
 	rot <- roteye(origin, center, column)
 	idx <- list(1:3,4:6, 7:9)[[column]]
 	data <- as.matrix(data[,idx])
@@ -147,6 +148,16 @@ pointsXYZ <- function(data, center, column=1) {
 
 plot.SO3 <- function(x, center=mean(x), col=1, to_range=FALSE, show_estimates=NULL, label_points=NULL, mean_regions=NULL, median_regions=NULL, alp=NULL, m=300,  ...) {
 
+  if(col=='all'){
+    p1<-plot(x,center=center,col=1,to_range=to_range,show_estimates=show_estimates, label_points=label_points, mean_regions=mean_regions, median_regions=median_regions, alp=alp, m=m,  ...)
+    p1<-p1+theme(axis.title.x=element_text(size=rel(1.5)))+xlab("x-axis")
+    p2<-plot(x,center=center,col=2,to_range=to_range,show_estimates=show_estimates, label_points=label_points, mean_regions=mean_regions, median_regions=median_regions, alp=alp, m=m,  ...)
+    p2<-p2+theme(axis.title.x=element_text(size=rel(1.5)))+xlab("y-axis")
+    p3<-plot(x,center=center,col=3,to_range=to_range,show_estimates=show_estimates, label_points=label_points, mean_regions=mean_regions, median_regions=median_regions, alp=alp, m=m,  ...)
+    p3<-p3+theme(axis.title.x=element_text(size=rel(1.5)))+xlab("z-axis")
+    multiplot(p1,p2,p3,cols=3)
+  }
+  
   Rs <- as.SO3(x)
 	xlimits <- c(-1,1)
 	ylimits <- c(-1,1)
@@ -267,3 +278,50 @@ plot.SO3 <- function(x, center=mean(x), col=1, to_range=FALSE, show_estimates=NU
 		xlim(xlimits) + ylim(ylimits)
 }
 
+# Multiple plot function
+# 
+# Taken from http://www.cookbook-r.com/Graphs/Multiple_graphs_on_one_page_(ggplot2)/
+#
+# ggplot objects can be passed in ..., or to plotlist (as a list of ggplot objects)
+# - cols:   Number of columns in layout
+# - layout: A matrix specifying the layout. If present, 'cols' is ignored.
+#
+# If the layout is something like matrix(c(1,2,3,3), nrow=2, byrow=TRUE),
+# then plot 1 will go in the upper left, 2 will go in the upper right, and
+# 3 will go all the way across the bottom.
+#
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  #require(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
