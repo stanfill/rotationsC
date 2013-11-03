3e9d9c5629a0763754fee2b8199a5f434a50a54b
Bryan Stanfill <stanfill@iastate.edu>
2013-11-02 20:30:44 -0500

    add fisher g function, run larger simulations

--- intervals/BayesRegions/BayesRegionSimulation.R
+++ intervals/BayesRegions/BayesRegionSimulation.R
@@ -1,14 +1,42 @@
 source("intervals/BayesRegions/BayesRegionsFunctions.R")
 
 
+#############
+#von Mises Distribution
 kap<-1
 Rs<-ruars(100,rvmises,kappa=kap)
 
 #Table 1 in Bingham 2009 suggests rho=1000, sigma=1 when for sample n=100 and kappa=1
-mcRes<-both_MCMC(Rs,mean(Rs),kappa0=1,rho=1000,sigma=1,50,100)
+mcRes<-both_MCMC(Rs,mean(Rs),kappa0=1,rho=1000,sigma=1,burnin=100,B=500,gfun=gvmUARS)
 
 Sres<-as.SO3(mcRes$S)
 plot(Sres)
 ares<-angle(Sres)
-plot(ares,type='l')
-plot(mcRes$kappa,type='l')
+plot(ares,type='l',ylab='r')
+plot(mcRes$kappa,type='l',ylab=expression(kappa))
+
+#Make CR, see if id.SO3 is in there
+Shat<-mean(Sres)
+ds<-afun(Sres,Shat)
+rad<-quantile(ds,.95)
+afun(id.SO3,Shat)<rad
+
+#############
+#matrix Fisher Distribution
+kap<-1
+Rs<-ruars(100,rfisher,kappa=kap)
+
+#Table 2 in Bingham 2010 suggests phi=1000, sigma=1 when for sample n=100 and kappa=1
+mcRes<-both_MCMC(Rs,mean(Rs),kappa0=kap,rho=1000,sigma=1,burnin=50,B=100,gfun=gfUARS)
+
+Sres<-as.SO3(mcRes$S)
+plot(Sres)
+ares<-angle(Sres)
+plot(ares,type='l',ylab='r')
+plot(mcRes$kappa,type='l',ylab=expression(kappa))
+
+#Make CR, see if id.SO3 is in there
+Shat<-mean(Sres)
+ds<-afun(Sres,Shat)
+rad<-quantile(ds,.95)
+afun(id.SO3,Shat)<rad
--- intervals/BayesRegions/BayesRegionsFunctions.R
+++ intervals/BayesRegions/BayesRegionsFunctions.R
@@ -3,6 +3,7 @@
 #Application to Materials Science
 
 gvmUARS<-function(RS,S,kappa){
+  #Compute equation (6) of Bingham et al. 2009b
   
   #Rs - n-by-9 matrix with rows corresponding to sample points
   #S - central orientation
@@ -23,18 +24,40 @@ gvmUARS<-function(RS,S,kappa){
   
 }
 
-S_MCMC<-function(Rs,oldS,rho,kappa){
+gfUARS<-function(RS,S,kappa){
+  #Compute equation (13) of Bingham et al. 2010
+  
+  #Rs - n-by-9 matrix with rows corresponding to sample points
+  #S - central orientation
+  #kappa - concentration parameter
+  
+  cRs <- center(Rs,S) #Each row of cRs is S'R
+  trcRs <- rowSums(cRs[,c(1,5,9)]) #each row is tr(S'R)
+  
+  n1<-exp(kappa*sum(trcRs-1))
+  I02k<-besselI(2*kappa,0)
+  I12k<-besselI(2*kappa,1)
+  
+  n2<-sqrt(2*I02k^2/kappa-2*I02k*I12k/(kappa^2)+((1/(kappa^2))-(2/kappa))*I12k^2)
+  
+  d1<-(I02k-I12k)^(nrow(Rs)+1)
+  
+  return((n1*n2)/(d1))
+  
+}
+
+S_MCMC<-function(Rs,oldS,rho,kappa,gfun){
   
   #Rs - the sample
   #oldS - the previous draw from distribution on S
   #rho - tuning parameter
   #kappa - concentration for likelihood
+  #gfun - the g() function, either gvmUARS or gfUARS
   
   Sstar <- ruars(1,rvmises,S=oldS,kappa=rho)
   
-  rj1 <- gvmUARS(Rs,Sstar,kappa)/gvmUARS(Rs,oldS,kappa)
- 
-  Wj1 <-rbinom(1,1,min(1,rj1))
+  rj1 <- gfun(Rs,Sstar,kappa)/gfun(Rs,oldS,kappa)
+  Wj1 <-rbinom(1,1,min(1,rj1,na.rm=T))
  
   if(Wj1==1){
     return(Sstar)
@@ -42,18 +65,19 @@ S_MCMC<-function(Rs,oldS,rho,kappa){
     return(oldS)
 }
 
-kap_MCMC<-function(Rs,oldKappa,sigma,S){
+kap_MCMC<-function(Rs,oldKappa,sigma,S,gfun){
   
   #Rs - the sample
   #oldKappa - the previous draw from distribution for kappa
   #sigma - tuning parameter
   #kappa - central orientation for likelihood
+  #gfun - the g() function, either gvmUARS or gfUARS
   
   kappaStar <- exp(rnorm(1,log(oldKappa),sigma))
   
-  rj2 <- gvmUARS(Rs,S,kappaStar)/gvmUARS(Rs,S,oldKappa)
+  rj2 <- (kappaStar*gfun(Rs,S,kappaStar))/(oldKappa*gfun(Rs,S,oldKappa))
   
-  Wj2 <- rbinom(1,1,min(1,rj2))
+  Wj2 <- rbinom(1,1,min(1,rj2,na.rm=T))
   
   if(Wj2==1){
     return(kappaStar)
@@ -64,7 +88,7 @@ kap_MCMC<-function(Rs,oldKappa,sigma,S){
 }
 
 
-both_MCMC<-function(Rs,S0,kappa0,rho,sigma,burnin,B){
+both_MCMC<-function(Rs,S0,kappa0,rho,sigma,burnin,B,gfun){
   
   Sdraws<-matrix(0,B,9)
   Kdraws<-rep(0,B)
@@ -73,8 +97,8 @@ both_MCMC<-function(Rs,S0,kappa0,rho,sigma,burnin,B){
   Knew<-kappa0
   
   for(i in 1:(burnin+1)){
-    Snew<-S_MCMC(Rs,Snew,rho,Knew)
-    Knew<-kap_MCMC(Rs,Knew,sigma,Snew)
+    Snew<-S_MCMC(Rs,Snew,rho,Knew,gfun)
+    Knew<-kap_MCMC(Rs,Knew,sigma,Snew,gfun)
   }
   
   Sdraws[1,]<-Snew
@@ -82,12 +106,28 @@ both_MCMC<-function(Rs,S0,kappa0,rho,sigma,burnin,B){
   
   for(j in 2:B){
     Sold<-as.SO3(matrix(Sdraws[(j-1),]))
-    Sdraws[j,]<-S_MCMC(Rs,Sold,rho,Kdraws[j-1])
+    Sdraws[j,]<-S_MCMC(Rs,Sold,rho,Kdraws[j-1],gfun)
     
     Snew<-as.SO3(matrix(Sdraws[(j),]))
-    Kdraws[j]<-kap_MCMC(Rs,Kdraws[j-1],sigma,Snew)
+    Kdraws[j]<-kap_MCMC(Rs,Kdraws[j-1],sigma,Snew,gfun)
   }
   
   return(list(S=Sdraws,kappa=Kdraws))
+  
+}
+
+#Compute the angles between each axis of R1 and R2
+afun<-function(R1,R2){
+  
+  n<-length(R1)/9
+  R1<-matrix(R1,n,9)
+  R2<-matrix(R2,3,3)
+  as<-rep(0,n)
+  
+  for(i in 1:n){
+    Ri<-matrix(R1[i,],3,3)
+    as[i]<-max(acos(diag(t(Ri)%*%R2)))
+  }
+  return(as)
   
 }
