e4d275d8ae07b9544150d4252864076841781515
Bryan Stanfill <stanfill@iastate.edu>
2013-11-04 11:35:20 -0600

    C++ version of S MCMC

--- intervals/BayesRegions/CheckCppFuns.R
+++ intervals/BayesRegions/CheckCppFuns.R
@@ -4,16 +4,15 @@ library(microbenchmark)
 Rs<-ruars(20,rcayley)
 cRs<-center(Rs,mean(Rs))
 cRs2<-centerCpp(Rs,mean(Rs))
+microbenchmark(center(Rs,mean(Rs)),centerCpp(Rs,mean(Rs)))
 
-sum(abs(cRs-cRs2))
+
+sum(abs(matrix(cRs,20,9)-matrix(cRs2,20,9)))
 mean(as.SO3(cRs2))
 
 microbenchmark(center(Rs,mean(Rs)),centerCpp(Rs,mean(Rs)))
 
 rowSums(cRs[,c(1,5,9)])
-gvmUARSC(Rs,mean(Rs),10.1)
-
-
 
 gvmUARS(Rs,mean(Rs),1)
 gvmUARSC(Rs,mean(Rs),1)
@@ -26,3 +25,7 @@ microbenchmark(gcayUARS(Rs,mean(Rs),1),gcayUARSC(Rs,mean(Rs),1))
 gcayUARS(Rs,mean(Rs),10)
 gcayUARSC(Rs,mean(Rs),10)
 microbenchmark(gcayUARS(Rs,mean(Rs),1),gcayUARSC(Rs,mean(Rs),1))
+
+S_MCMC_CPP(Rs,mean(Rs),1000,1)
+S_MCMC(Rs,mean(Rs),1000,1,gcayUARSC,rcayley)
+microbenchmark(S_MCMC(Rs,mean(Rs),1000,1,gcayUARSC,rcayley),S_MCMC_CPP(Rs,mean(Rs),1000,1))
--- intervals/BayesRegions/CppBayesFunctions.cpp
+++ intervals/BayesRegions/CppBayesFunctions.cpp
@@ -3,11 +3,10 @@
 
 using namespace Rcpp;
 
-
 // [[Rcpp::export]]
 arma::mat centerCpp(arma::mat Rs, arma::mat S){
-  //Center the dataset Rs around S, so each row of Rs denoted R is replaces with S'R
   
+  //Center the dataset Rs around S, so each row of Rs denoted R is replaces with S'R
   int n = Rs.n_rows;
   arma::mat Rsi(3,3), cRs(n,9);
   int i = 0, j=0;
@@ -106,4 +105,95 @@ double gcayUARSC(arma::mat Rs, arma::mat S, double kappa){
   return p1*p2*p4;
  
 }
+
+
+// [[Rcpp::export]]
+arma::mat eskewC(arma::rowvec U) {
+  
+  double ulen=norm(U,2);
+  
+  U = U/ulen;
+  
+  double u = U(0);
+  double v = U(1);
+  double w = U(2);
+  
+  arma::mat33 res;
+  res.zeros();
+  res(0,1) = -w;
+  res(1,0) = w;
+  res(0,2) = v;
+  res(2,0) = -v;
+  res(1,2) = -u;
+  res(2,1) = u;
+  
+  return res;
+}
+
+arma::mat genrC(arma::mat S,double r) {
+  
+  RNGScope scope;
+
+  NumericVector ta = runif(1,-1,1);
+  double theta = ta[0];
+  theta = acos(theta);
+    
+  NumericVector ph = runif(1, -M_PI, M_PI);
+  double phi = ph[0];
+  
+  arma::rowvec u(3);
+  
+  u(0)=sin(theta) * cos(phi);
+  u(1)=sin(theta) * sin(phi);
+  u(2)=cos(theta);
+
+  arma::mat Ri, I(3,3), SS;
+  I.eye();
+  Ri = u.t() * u;
+  SS = eskewC(u);
+  Ri = Ri + (I - Ri) * cos(r) +  SS * sin(r);
+  Ri = S * Ri;
+  return Ri;
+
+}
+
+double rcayleyCpp(double kappa){
+  RNGScope scope;
+  NumericVector bet(1), alp(1);
+  
+  bet = rbeta(1,kappa+0.5,1.5);
+  alp = rbinom(1,1,0.5);
+ 
+  double theta = acos(2*bet[0]-1)*(1-2*alp[0]);
+  return theta;
+}
+
+// [[Rcpp::export]]
+arma::mat S_MCMC_CPP(arma::mat Rs, arma::mat oldS, double rho, double kappa){
+  RNGScope scope;
+  
+  double r, rj1, prob;
+  NumericVector W1(1);
+  r = rcayleyCpp(rho);
+  arma::mat Sstar = genrC(oldS,r);
+  
+  rj1 = gcayUARSC(Rs,Sstar,kappa);
+  rj1 /= gcayUARSC(Rs,oldS,kappa);
+  
+  if(rj1>1){
+    rj1 = 1;
+  }
+  
+  W1=rbinom(1,1,rj1);
+  
+  if(W1[0]==1){
+    return Sstar;
+  }else{
+    return oldS;
+  }
+  
+}
+
+// [[Rcpp::export]]
+
 
