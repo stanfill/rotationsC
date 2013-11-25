246533bb0d4db187a0c6d1e49083715c6502c8cf
Bryan Stanfill <stanfill@iastate.edu>
2013-11-25 16:41:46 -0600

    write code for median paper data example

---
+++ intervals/MedianPaper/DataExample_Median.R
@@ -0,0 +1,76 @@
+############
+#Try the nickel data of Bingham's Disertation
+############
+source("intervals/MedianPaper/PrepareData.R")
+
+#Plot grain map
+qplot(xpos,ypos,colour=dE1,data=loc.stats,size=I(5))
+
+#Grain map based on median off all 14 scans
+d <- ggplot(loc.stats, aes(xpos, ypos, color=dE1))
+d2 <- d + geom_point(size=2.5) + scale_colour_gradient(expression(d[R](tilde(S)[E], I["3x3"])), low="grey99", high="grey10", limits=c(0, pi), breaks=c( pi/4, pi/2, 3*pi/4), labels=expression( pi/4, pi/2, 3*pi/4)) + 
+  theme_bw() + xlab("") + ylab("") + coord_equal() + scale_x_continuous(limits=c(0, 12.5), breaks=seq(0, 12.5, by=2.5), labels=expression(0*mu*m, 2.5*mu*m, 5*mu*m, 7.5*mu*m, 10*mu*m, 12.5*mu*m)) + 
+  scale_y_continuous(limits=c(0, 10), breaks=seq(0, 10, by=2.5), labels=expression(0*mu*m, 2.5*mu*m, 5*mu*m, 7.5*mu*m, 10*mu*m)) + 
+  #geom_point(shape="o", colour="yellow", size=5, data=loc.stats[idx,])  + 
+  theme(plot.margin=unit(rep(0,4), "lines"))
+d2
+#ggsave(file="/Users/stanfill/Dropbox/Thesis/Intervals/Figures/grain-map.png", width=5.75, height=4.25)
+
+#####################
+#### Draw eyeballs for worst sample to illustrate mean/median difference
+#####################
+
+#Find the location where the mean and median disagree most, this is likely a "contaminated" sample
+ex<-loc.stats[which.max(loc.stats$dE),]$location #Should be 698
+  
+exRots<-as.SO3(data.matrix(dat.out[dat.out$location==ex,3:11]))
+#exRots<-as.SO3(exRots[-4,]) #potentially remove the observation that isn't stricly a rotation
+p1<-plot(exRots,center=median(exRots),show_estimates=c('proj.mean','proj.median'),median_regions='bootstrap',mean_regions='moment bootstrap',alp=.1,m=300)
+p2<-plot(exRots,center=median(exRots),show_estimates=c('proj.mean','proj.median'),median_regions='bootstrap',mean_regions='moment bootstrap',alp=.1,m=300,col=2)
+p3<-plot(exRots,center=median(exRots),show_estimates=c('proj.mean','proj.median'),median_regions='bootstrap',mean_regions='moment bootstrap',alp=.1,m=300,col=3)
+
+ps<-list(p1+theme(legend.position='none'),p2+theme(legend.position='none'),p3+theme(legend.position='none'))
+multiplot(plotlist=ps,layout=matrix(c(1:3),nrow=1,byrow=T))
+#ggsave doesn't work, use "Export." I used "width=900" and "height=300"
+
+#Add legend back if necessairy
+legend <- g_legend(p1) #pull the legend off--doesn't work
+psLeg<-list(p1+theme(legend.position='none'),p2+theme(legend.position='none'),p3+theme(legend.position='none'),legend)
+multiplot(plotlist=psLeg,layout=matrix(c(1:4),nrow=1,byrow=T))
+
+#####################
+#### Find all locations where mean/median differ substantially
+#####################
+
+#Randomly select a location with enough spread to be interesting
+possibles<-which(loc.stats$dE>.1 & loc.stats$dE<.15)
+m<-length(possibles)
+DataExDF<-data.frame(Location=rep(0,m),MeanNTH=rep(0,m),MedianNTH=rep(0,m),MeanBoot=rep(0,m),MedianBoot=rep(0,m))
+
+for(i in 1:m){
+  ex<-loc.stats[possibles[i],]$location 
+  DataExDF$Location[i]<-ex
+  exRots<-as.SO3(data.matrix(dat.out[dat.out$location==ex,3:11]))
+  DataExDF$MeanNTH[i]<-region(exRots,method='moment',type='theory',estimator='mean',alp=.9)*180/pi
+  DataExDF$MedianNTH[i]<-region(exRots,method='moment',type='theory',estimator='median',alp=.9)*180/pi
+  DataExDF$MeanBoot[i]<-region(exRots,method='moment',type='bootstrap',estimator='mean',alp=.9,m=100)*180/pi
+  DataExDF$MedianBoot[i]<-region(exRots,method='moment',type='bootstrap',estimator='median',alp=.9,m=100)*180/pi
+}
+
+library(xtable)
+xtable(DataExDF,digits=3)
+
+
+#####################
+#### Find all locations where mean/median differ substantially, highlight them in grain map
+#####################
+
+#Grain map based on median off all 14 scans
+d <- ggplot(loc.stats, aes(xpos, ypos, color=dE1))
+d2 <- d + geom_point(size=4) + scale_colour_gradient(expression(d[R](tilde(S)[E], I["3x3"])), low="grey99", high="grey10", limits=c(0, pi), breaks=c( pi/4, pi/2, 3*pi/4), labels=expression( pi/4, pi/2, 3*pi/4)) + 
+  theme_bw() + xlab("") + ylab("") + coord_equal() + scale_x_continuous(limits=c(0, 12.5), breaks=seq(0, 12.5, by=2.5), labels=expression(0*mu*m, 2.5*mu*m, 5*mu*m, 7.5*mu*m, 10*mu*m, 12.5*mu*m)) + 
+  scale_y_continuous(limits=c(0, 10), breaks=seq(0, 10, by=2.5), labels=expression(0*mu*m, 2.5*mu*m, 5*mu*m, 7.5*mu*m, 10*mu*m)) + 
+  geom_point(shape="o", colour="yellow", size=5, data=loc.stats[possibles,])  + 
+  theme(plot.margin=unit(rep(0,4), "lines"))
+d2
+ggsave("/Users/stanfill/Dropbox/Thesis/Intervals - Median/Figures/Grain_map_with_circles.pdf",height=8,width=9)
---
+++ intervals/MedianPaper/PrepareData.R
@@ -0,0 +1,100 @@
+#This gets the dataset in a unable condition
+
+library(rotations)
+library(plyr)
+library(reshape2)
+library(gridExtra)
+#Plot multiple plots on one
+multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+  #require(grid)
+  
+  # Make a list from the ... arguments and plotlist
+  plots <- c(list(...), plotlist)
+  
+  numPlots = length(plots)
+  
+  # If layout is NULL, then use 'cols' to determine layout
+  if (is.null(layout)) {
+    # Make the panel
+    # ncol: Number of columns of plots
+    # nrow: Number of rows needed, calculated from # of cols
+    layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+                     ncol = cols, nrow = ceiling(numPlots/cols))
+  }
+  
+  if (numPlots==1) {
+    print(plots[[1]])
+    
+  } else {
+    # Set up the page
+    grid.newpage()
+    pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+    
+    # Make each plot, in the correct location
+    for (i in 1:numPlots) {
+      # Get the i,j matrix positions of the regions that contain this subplot
+      matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+      
+      print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+                                      layout.pos.col = matchidx$col))
+    }
+  }
+}
+
+#g_legend will strip the legend and add it back so they can share one legend
+g_legend<-function(a.gplot){
+  tmp <- ggplot_gtable(ggplot_build(a.gplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
+}
+
+load("/Users/stanfill/Dropbox/Rotation matrices/Melissa data/datasetnickel.RData")
+
+dat.out <- adply(data, .margins= c(1,3), function(x) {
+  as.vector(x)
+})
+names(dat.out)[c(1,2)] <- c("rep", "location")
+dat.out$xpos <- xpos[dat.out$location]
+dat.out$ypos <- ypos[dat.out$location]
+## convert data into a data frame including location and position information
+#head(dat.out)
+
+## are the matrices actually rotations?
+checks <- adply(dat.out, .margins=1, function(x) {
+  is.SO3(unlist(x[3:11]))
+})
+dat.out$check <- checks$V1
+dat.out<-dat.out[dat.out$check==T,]
+## compute the projected estimators at each location
+dat.ests <- dlply(dat.out, .(location), function(x) {
+  res <- na.omit(x)
+  res <- subset(res, check==TRUE)
+  
+  n <- nrow(res) 
+  R1 <- SE2  <- SE1  <- NULL
+  if (n == 1) {
+    R <- as.SO3(matrix(unlist(res[1,3:11]), 3, 3))
+    R1 <- SE2  <- SE1  <- R
+  } else if (n > 0) {
+    rots <- as.SO3(as.matrix(res[,3:11]))
+    SE2 <- mean(rots)
+    SE1 <- median(rots)
+    R1 <- as.SO3(as.matrix(res[1,3:11]))
+  }
+  
+  location <- as.numeric(as.character(unique(x$location)))
+  return(list(location=location, n=n, SE2=SE2, SE1=SE1, R1=R1))
+})
+
+## find distances between estimators and angles to identity for each
+loc.stats <- ldply(dat.ests, function(x) {  
+  location <- as.numeric(as.character(unique(x$location)))
+  if (x$n > 0)
+    data.frame(location=x$location, n=x$n, 
+               dE1=angle(x$SE1), dE2=angle(x$SE2),
+               dE=dist(x$SE1, x$SE2, method="intrinsic", p=1),dR1=angle(x$R1)
+    )
+})
+loc.stats$xpos <- xpos[loc.stats$location]
+loc.stats$ypos <- ypos[loc.stats$location]
