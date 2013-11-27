c639c920b78223f70ae9d6db6782f1bc5f089e36
Bryan Stanfill <stanfill@iastate.edu>
2013-11-27 16:02:01 -0600

    plot standardized (by AV) IF too, make better plots

--- intervals/MedianPaper/InfluenceFunctions.R
+++ intervals/MedianPaper/InfluenceFunctions.R
@@ -1,26 +1,39 @@
-IFMean<-function(r,kap,Fisher=T){
+IFMean<-function(r,kap,Fisher=T,SIF=F){
+  
+  #SIF - Standardize by Asymptotic variance?
   
   if(Fisher){
     d<-(((kap+1)/kap)*besselI(2*kap,1)-besselI(2*kap,0))/(3*(besselI(2*kap,0)-besselI(2*kap,1)))
+    c<-(((kap+1)/kap)*besselI(2*kap,1)-besselI(2*kap,0))/(3*kap*(besselI(2*kap,0)-besselI(2*kap,1)))
   }else{
     d<-kap/(kap+2)
+    c<-(4*kap+2)/(kap^2+5*kap+6)
   }
-  return(sin(r)/d)
   
+  if(SIF){
+    return(2*d*sin(r)/c)
+  }else{
+    return(sin(r)/d)
+  }
 }
 
-IFMedian<-function(r,kap,Fisher=T){
+IFMedian<-function(r,kap,Fisher=T,SIF=F){
   fn<-sin(r)/sqrt(1-cos(r))
   
   if(Fisher){
     d<-exp(2*kap)*(6*sqrt(kap)-(3+8*kap)*dawson(2*sqrt(kap)))/(24*sqrt(2)*pi*kap^1.5*(besselI(2*kap,0)-besselI(2*kap,1)))
+    c<-besselI(2*kap,1)/(12*kap*(besselI(2*kap,0)-besselI(2*kap,1)))
   }else{
     d<-sqrt(2/pi)*kap*gamma(kap+2)/(3*gamma(kap+2.5))
+    c<-(2*kap+1)/(6*kap+12)
   }
   
-  return(fn/(2*d))
+  if(SIF){
+    return(d*fn/c)
+  }else{
+    return(fn/(2*d))
+  }
 }
-
 
 #############
 #Compare IFs
@@ -43,34 +56,80 @@ library(plyr)
 rs<-seq(-pi,pi,length=100)
 kap<-c(1,5,20)
 
-IFDF<-data.frame(r=rep(rs,3),kappa=rep(kap,each=100),Mean=c(IFMean(rs,kap[1],F),IFMean(rs,kap[2],F),IFMean(rs,kap[3],F)),
+IFDF<-data.frame(r=rep(rs,3),kappa=as.factor(rep(kap,each=100)),Mean=c(IFMean(rs,kap[1],F),IFMean(rs,kap[2],F),IFMean(rs,kap[3],F)),
                      Median=c(IFMedian(rs,kap[1],F),IFMedian(rs,kap[2],F),IFMedian(rs,kap[3],F)))
 
-IFDF$kappa<-as.factor(IFDF$kappa)
+#Plot mean and median seperately
 qplot(r,Mean,data=IFDF,geom='line',colour=kappa,group=kappa,ylab="IF-Mean")+theme_bw()+
   geom_vline(xintercept=0,colour='gray50')+geom_hline(yintercept=0,colour='gray50')
 
 qplot(r,Median,data=IFDF,geom='line',colour=kappa,group=kappa,ylab="IF-Median")+theme_bw()+
   geom_vline(xintercept=0,colour='gray50')+geom_hline(yintercept=0,colour='gray50')
 
-#Put all on one plot
+#Put both estimators on one plot
 IFDFm<-melt(IFDF,id=c("r","kappa"))
 colnames(IFDFm)[3:4]<-c("Estimator","IF")
 qplot(r,IF,data=IFDFm,geom='line',colour=kappa,linetype=Estimator)+theme_bw()+
   geom_vline(xintercept=0,colour='gray50')+geom_hline(yintercept=0,colour='gray50')
 
+#Facet by estimator
 qplot(r,IF,data=IFDFm,geom='line',colour=kappa,facets=.~Estimator)+theme_bw()+
   geom_vline(xintercept=0,colour='gray50')+geom_hline(yintercept=0,colour='gray50')
 
-qplot(r,IF,data=IFDFm,geom='line',colour=Estimator)+facet_grid(.~kappa, labeller = label_bquote(kappa==.(x)))+theme_bw()+
-  geom_vline(xintercept=0,colour='gray50')+geom_hline(yintercept=0,colour='gray50')+theme(aspect.ratio=1)
+#Facet by concentration
+qplot(r,IF,data=IFDFm,geom='line',colour=Estimator,ylab='IF(r,Cayley)',size=I(1.25))+theme_bw()+geom_vline(xintercept=0,colour='gray50')+
+  geom_hline(yintercept=0,colour='gray50')+theme(aspect.ratio=1)+facet_grid(.~kappa, labeller = label_bquote(kappa==.(x)))
+#ggsave("/Users/stanfill/Dropbox/Thesis/Intervals - Median/Figures/CayleyIF.pdf",width=4,height=6,units="in")
 
 ### Same with Fisher
 kap<-c(1,2.5,5)
-IFDFFish<-data.frame(r=rep(rs,3),kappa=rep(kap,each=100),Mean=c(IFMean(rs,kap[1],T),IFMean(rs,kap[2],T),IFMean(rs,kap[3],T)),
+IFDFFish<-data.frame(r=rep(rs,3),kappa=as.factor(rep(kap,each=100)),Mean=c(IFMean(rs,kap[1],T),IFMean(rs,kap[2],T),IFMean(rs,kap[3],T)),
                  Median=c(IFMedian(rs,kap[1],T),IFMedian(rs,kap[2],T),IFMedian(rs,kap[3],T)))
-IFDFFish$kappa<-as.factor(IFDFFish$kappa)
+
 IFDFFishm<-melt(IFDFFish,id=c("r","kappa"))
 colnames(IFDFFishm)[3:4]<-c("Estimator","IF")
-qplot(r,IF,data=IFDFFishm,geom='line',colour=Estimator)+facet_grid(.~kappa, labeller = label_bquote(kappa==.(x)))+theme_bw()+
-  geom_vline(xintercept=0,colour='gray50')+geom_hline(yintercept=0,colour='gray50')+theme(aspect.ratio=1)
+
+qplot(r,IF,data=IFDFFishm,geom='line',colour=Estimator,ylab='IF(r,Fisher)',size=I(1.25))+theme_bw()+geom_vline(xintercept=0,colour='gray50')+
+  geom_hline(yintercept=0,colour='gray50')+theme(aspect.ratio=1)+facet_grid(.~kappa, labeller = label_bquote(kappa==.(x)))
+#ggsave("/Users/stanfill/Dropbox/Thesis/Intervals - Median/Figures/FisherIF.pdf",width=4,height=6,units="in")
+
+
+#############
+#Compare SIFs as a function of kappa, Cayley
+library(ggplot2)
+library(reshape2)
+library(plyr)
+rs<-seq(-pi,pi,length=100)
+kap<-c(1,5,20)
+
+SIFDF<-data.frame(r=rep(rs,3),kappa=as.factor(rep(kap,each=100)),Mean=c(IFMean(rs,kap[1],F,T),IFMean(rs,kap[2],F,T),IFMean(rs,kap[3],F,T)),
+                 Median=c(IFMedian(rs,kap[1],F,T),IFMedian(rs,kap[2],F,T),IFMedian(rs,kap[3],F,T)))
+SIFDFm<-melt(IFDF,id=c("r","kappa"))
+colnames(SIFDFm)[3:4]<-c("Estimator","SIF")
+#Facet by concentration
+qplot(r,SIF,data=SIFDFm,geom='line',colour=Estimator,ylab='SIF(r,Cayley)')+theme_bw()+geom_vline(xintercept=0,colour='gray50')+
+  geom_hline(yintercept=0,colour='gray50')+theme(aspect.ratio=1)+facet_grid(.~kappa, labeller = label_bquote(kappa==.(x)))
+
+
+#Same For Fisher
+SIFDF<-data.frame(r=rep(rs,3),kappa=as.factor(rep(kap,each=100)),Mean=c(IFMean(rs,kap[1],T,T),IFMean(rs,kap[2],T,T),IFMean(rs,kap[3],T,T)),
+                  Median=c(IFMedian(rs,kap[1],T,T),IFMedian(rs,kap[2],T,T),IFMedian(rs,kap[3],T,T)))
+SIFDFm<-melt(IFDF,id=c("r","kappa"))
+colnames(SIFDFm)[3:4]<-c("Estimator","SIF")
+#Facet by concentration
+qplot(r,SIF,data=SIFDFm,geom='line',colour=Estimator,ylab='SIF(r,Fisher)')+theme_bw()+geom_vline(xintercept=0,colour='gray50')+
+  geom_hline(yintercept=0,colour='gray50')+theme(aspect.ratio=1)+facet_grid(.~kappa, labeller = label_bquote(kappa==.(x)))
+
+#############
+#Compare SIFs as a function of kappa standardized by KL divergence, Fisher
+library(ggplot2)
+library(reshape2)
+library(plyr)
+rs<-seq(-pi,pi,length=100)
+
+kap<-c(1,2.5,5)
+KF<-sqrt(kap*besselI(kap,0)/besselI(kap,1))
+IFDFFish<-data.frame(r=rep(rs,3),kappa=as.factor(rep(kap,each=100)),Mean=c(IFMean(rs,kap[1],T),IFMean(rs,kap[2],T),IFMean(rs,kap[3],T)),
+                     Median=c(IFMedian(rs,kap[1],T),IFMedian(rs,kap[2],T),IFMedian(rs,kap[3],T)))
+IFDFFish
+
