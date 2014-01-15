49ab8fe73e13785e4dfb0c624288c6b04212e0f3
Bryan Stanfill <stanfill@iastate.edu>
2014-01-14 22:00:02 -0600

    update grid.search example and log.SO3 man file

--- rotations/R/preliminary.R
+++ rotations/R/preliminary.R
@@ -385,6 +385,20 @@ skew.exp <- function(x) {
 #' @cite moakher02
 #' @S3method log SO3
 #' @method log SO3
+#' @examples
+#' Rs <- ruars(20, rcayley)
+#' 
+#' #Here we demonstrate how the logarithm can be used to determine the angle and 
+#' #axis corresponding to the provided sample
+#' 
+#' lRs <- log(Rs)               #Take the logarithm of the sample
+#' Ws <- lRs[,c(6, 7, 2)]       #The appropriate diagonal entries are the axis*angle
+#' lens <- sqrt(rowSums(Ws^2))  
+#' axes <- mis.axis(Rs)
+#' angs <- mis.angle(Rs)
+#' all.equal(axes, Ws/lens)
+#' all.equal(angs, lens)
+
 
 log.SO3 <- function(x,...) {
   if(length(x)==9){
