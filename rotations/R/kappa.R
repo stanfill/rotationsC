a89a4b856e93bdfa4f5f47ef66325f50b28fc585
stanfill <bryan.stanfill@csiro.au>
2015-09-29 13:44:16 +1000

    add kappa->nu translation for maxwell dist

--- rotations/R/kappa.R
+++ rotations/R/kappa.R
@@ -96,3 +96,41 @@ vmises.kappa<-function(nu){
   
   return(kappa)
 }
+
+
+#This goes from kappa to nu, need to incorporate nu
+maxwell.nu.kappa <- function(kappa,nu){
+  ((1-(2*sqrt(kappa*pi)*exp(-kappa*pi*pi)+(2*kappa-1)*exp(-1/(4*kappa))/(2*kappa)))-nu)^2
+}
+
+
+#'  Circular variance and concentration parameter
+#'  
+#'  Return the concentration parameter that corresponds to a given circular variance.
+#'  
+#'  The concentration parameter \eqn{\kappa} does not translate across circular distributions.  A commonly used
+#'  measure of spread in circular distributions that does translate
+#'  is the circular variance defined as \eqn{\nu=1-E[\cos(r)]}{\nu=1-E[cos(r)]} where \eqn{E[\cos(r)]}{E[cos(r)]} 
+#'  is the mean resultant length.  See \cite{mardia2000} for more details.
+#'  This function translates the circular variance \eqn{\nu} into the corresponding concentration parameter \eqn{\kappa}
+#'  for the modified Maxwell-Boltzmann distribution.
+#'  
+#'  @param nu circular variance
+#'  @return Concentration parameter corresponding to nu.
+#'  @seealso \code{\link{Maxwell}}
+#'  @export
+#'  @examples
+#'  #Find the concentration parameter for circular variances 0.25, 0.5, 0.75
+#'  maxwell.kappa(0.25)
+#'  maxwell.kappa(0.5)
+#'  maxwell.kappa(0.75)
+
+maxwell.kappa<-function(nu){
+  
+  kappa<-rep(0,length(nu))
+  
+  for(i in 1:length(nu))
+    kappa[i]<-optimize(maxwell.nu.kappa,interval=c(0,10),tol=.00001,nu=nu[i])$minimum
+  
+  return(kappa)
+}
