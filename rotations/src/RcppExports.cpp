c2058b9bd03d161cd567a08c2d8b37e2f7ea2e60
Bryan <bstanfill2003@gmail.com>
2022-06-24 14:54:56 -0500

    misc updates

--- rotations/src/RcppExports.cpp
+++ rotations/src/RcppExports.cpp
@@ -9,6 +9,11 @@
 
 using namespace Rcpp;
 
+#ifdef RCPP_USE_GLOBAL_ROSTREAM
+Rcpp::Rostream<true>&  Rcpp::Rcout = Rcpp::Rcpp_cout_get();
+Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
+#endif
+
 // rcayleyCpp
 Rcpp::NumericVector rcayleyCpp(unsigned int n, double kappa);
 static SEXP _rotations_rcayleyCpp_try(SEXP nSEXP, SEXP kappaSEXP) {
